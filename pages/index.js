89d5c5290c0e26f6bcb15266b1a6962c04fe2581
chakrahun <108547535+chakrahun@users.noreply.github.com>
2023-02-24 13:42:12 +1100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,4 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
-
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
-  )
-}
+<a class="weatherwidget-io" href="https://forecast7.com/en/n37d81144d96/melbourne/" data-label_1="Melbourne" data-label_2="Mood" data-font="Georgia" data-theme="dark" data-shadow="#64473A" data-accent="rgba(0, 0, 0, 0.12)" data-textcolor="#64473A" data-highcolor="#64473A" data-lowcolor="#64473A" data-suncolor="#64473A" data-mooncolor="#64473A" data-cloudcolor="#64473A" data-cloudfill="#64473A" data-raincolor="#64473A" data-snowcolor="#64473A" >Melbourne Mood</a>
+<script>
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+</script>
